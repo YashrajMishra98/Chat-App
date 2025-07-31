d7896398ff2382c6962d9cac35b8ba9ae211a2e9
Yashraj Mishra <yashrajmishra067@gmail.com>
2025-07-31 14:30:27 +0530

    Add Backend deployed path

--- cchat/src/component/Chat/Chat.js
+++ cchat/src/component/Chat/Chat.js
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = "https://demo-cchat.herokuapp.com/";
+const ENDPOINT = "https://chat-app-wnmf.onrender.com";
 
 const Chat = () => {
     const [id, setid] = useState("");
